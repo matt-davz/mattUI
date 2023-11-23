09bcf4b4092d4e552b793e9d110b16b7aeba53c5
matt-davz <davies.matt.02@gmail.com>
2023-11-23 08:44:32 +0700

    added ts compilers and configs

---
+++ component/Selector.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+export interface data {
+    img: string,
+    text: string
+}
+
+export interface selectedData {
+    img: string,
+    text: string,
+    amount: number
+}
+
+export interface SelectorProps {
+    onOkay?: () => selectedData,
+    onCancel?: () => void,
+    data: data[],
+};
+
+const Selector: React.FunctionComponent<SelectorProps> = (props) => {
+  const { onOkay, onCancel, data } = props
+
+  const [amount, setAmount] = useState<number>(0)
+  const [selected,setSelected] = useState<boolean>(false)
+  const [term,setTerm] = useState<string>("") //need condition so that if the term is empty it wikll not show all the data
+
+  return (
+    <div>Selector</div>
+  )
+}
+
+export default Selector
